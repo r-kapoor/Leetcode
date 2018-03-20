3fb1ce165ee65e4c1e99c196c4a4577732fc8ef4
Rahul Kapoor <rahulkapoor.bits@gmail.com>
2018-03-19 23:22:29 -0700

    Add RTree

--- src/java1/RadixTree.java
+++ src/java1/RadixTree.java
@@ -21,8 +21,18 @@ public class RadixTree {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RadixTree tree = new RadixTree();
+		tree.addWord("hello");
+		tree.addWord("hi");
+		tree.addWord("water");
+		tree.addWord("watery");
+		System.out.println(tree.findPrefix("hello"));
+		System.out.println(tree.findPrefix("hel"));
+		System.out.println(tree.findPrefix("h"));
+		System.out.println(tree.findPrefix("hi"));
+		System.out.println(tree.findPrefix("air"));
+		System.out.println(tree.findPrefix("wat"));
+		System.out.println(tree.findPrefix("watery"));
 	}
 	
 	public void addWord(String word) {
@@ -35,17 +45,64 @@ public class RadixTree {
 			return;
 		}
 		
-		char cur = word.charAt(index);
-		if(root.map.containsKey(cur)) {
-			// Character already in the map
-			addWord(word, index+1, root.map.get(cur));
+		String currWord = word.substring(index);
+		int i = 0;
+		String edgeWordMatched = null;
+		for(String edgeWords : root.map.keySet()) {
+			int minLength = Math.min(edgeWords.length(), currWord.length());
+			for(; i < minLength; i++) {
+				if(edgeWords.charAt(i) != currWord.charAt(i)) {
+					break;
+				}
+			}
+			if(i > 0) {
+				//Match
+				edgeWordMatched = edgeWords;
+				break;
+			}
+		}
+		if(i > 0) {
+			//Matched
+			String matched = edgeWordMatched.substring(0, i);
+			Node child = root.map.get(edgeWordMatched);
+			root.map.remove(edgeWordMatched);
+			Node newChild = new Node();
+			newChild.map.put(edgeWordMatched.substring(i), child);
+			root.map.put(matched, newChild);
+			addWord(word, index+i, newChild);
 		}
 		else {
-			// Add the character to the map
+			// Add the word to the map
 			Node node = new Node();
-			root.map.put(cur, node);
-			addWord(word, index+1, node);
+			root.map.put(currWord, node);
+			addWord(word, index+currWord.length(), node);
 		}
+	}
+	
+	public boolean findPrefix(String prefix) {
+		return findPrefix(prefix, 0, root);
+	}
+	
+	private boolean findPrefix(String prefix, int index, Node root) {
+		if(index >= prefix.length()) {
+			return true;
+		}
+		
+		String currWord = prefix.substring(index);
+		for(String edgeWords : root.map.keySet()) {
+			int minLength = Math.min(edgeWords.length(), currWord.length());
+			int i = 0;
+			for(; i < minLength; i++) {
+				if(edgeWords.charAt(i) != currWord.charAt(i)) {
+					break;
+				}
+			}
+			if(i > 0) {
+				//Match
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
